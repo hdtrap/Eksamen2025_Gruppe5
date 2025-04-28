b1374d4ccf22a6797053fa98ac17b786f1c139b0
hdtrap <peterpetertange@gmail.com>
2025-04-28 15:40:57 +0200

    :)

---
+++ src/main/java/org/example/eksamen2025_gruppe5/Eksamen2025Gruppe5Application.java
@@ -0,0 +1,13 @@
+package org.example.eksamen2025_gruppe5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen2025Gruppe5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamen2025Gruppe5Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eksamen2025_gruppe5/Eksamen2025Gruppe5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamen2025_gruppe5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen2025Gruppe5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

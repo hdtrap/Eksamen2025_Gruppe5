f88de445ddf1d0ae1c8de46ae2d8b704fc0f42ff
KEAmimi <mimi0001@stud.kea.dk>
2025-04-30 12:29:49 +0200

    Initial Commit
    Landing page returns hello world

---
+++ src/main/java/org/example/eksamen2025_gruppe5/controller/PageController.java
@@ -0,0 +1,13 @@
+package org.example.eksamen2025_gruppe5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String getLoginpage(){
+        return "index";
+    }
+}
--- src/test/java/org/example/eksamen2025_gruppe5/Eksamen2025Gruppe5ApplicationTests.java
+++ src/test/java/org/example/eksamen2025_gruppe5/Eksamen2025Gruppe5ApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Eksamen2025Gruppe5ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }

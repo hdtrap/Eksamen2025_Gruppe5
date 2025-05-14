808ef8306ec48bf464faf3c4aaa616166213ed0b
CoderFrede1010 <frfu0001@stud.kea.dk>
2025-05-14 14:46:10 +0200

    De er nu muligt at printe skadesrapporter

--- src/main/java/org/example/eksamen2025_gruppe5/controller/DamageController.java
+++ src/main/java/org/example/eksamen2025_gruppe5/controller/DamageController.java
@@ -2,6 +2,7 @@ package org.example.eksamen2025_gruppe5.controller;
 
 import org.example.eksamen2025_gruppe5.exceptions.LeaseNotFoundException;
 import org.example.eksamen2025_gruppe5.model.Damage;
+import org.example.eksamen2025_gruppe5.model.Lease;
 import org.example.eksamen2025_gruppe5.repository.DamageRepository;
 import org.example.eksamen2025_gruppe5.repository.LeaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 @Controller
@@ -116,8 +121,60 @@ public String getEditDamage(@RequestParam("damageId") int damageId,
     public String getShowDamage(@RequestParam("leaseId") int leaseId){
         return "redirect:/showDamage?leaseId=" + leaseId;
 }
-
-
-
-
+@GetMapping("/getPrintDamageReport")
+    public String getPrintDamageReport(@RequestParam("leaseId") int leaseId, Model model){
+    System.out.println("du er nu i getPrintDamageReport leaseid: " +leaseId);
+    model.addAttribute("leaseId", leaseId);
+    ArrayList<Damage> damages = damageRepository.getAllDamagesForALeaseWithLeaseId(leaseId);
+    model.addAttribute("damages", damages);
+        return "printDamageReport";
 }
+@GetMapping("/printDamageReport") // Denne metode printer skaderapporter i en mappe i systemet der hedder skaderapport
+    public String printDamageReport(@RequestParam("leaseId") int leaseId, Model model,
+                                    RedirectAttributes redirectAttributes) throws IOException {
+        model.addAttribute("leaseId", leaseId);
+     System.out.println("du er nu i printDamageReport leaseid: " +leaseId);
+
+    try {
+            Lease lease = leaseRepository.findLeaseById(leaseId);
+            ArrayList<Damage> damages = damageRepository.getAllDamagesForALeaseWithLeaseId(leaseId);
+            //Pathen hedder skaderapport da det er i den yderste mappe
+            String folderPath = "skaderapport";
+            File folder = new File(folderPath);
+            //Navnet på den fil der bliver lavet og dens path
+            String filePath = folderPath + "/skaderapport_" + leaseId + ".txt";
+            FileWriter fw = new FileWriter(filePath);
+
+            try (BufferedWriter writer = new BufferedWriter(fw)) {
+                writer.write("Skaderapport for lease ID: " + leaseId);
+                writer.newLine();
+                writer.write("Kunde: " + lease.getCustomerName());
+                writer.newLine();
+                writer.write("----------------------------------");
+                writer.newLine();
+
+                for (Damage damage : damages) {
+                    writer.write("Skade id: " + damage.getDamageId());
+                    writer.newLine();
+                    writer.write("Skade type: " + damage.getDamageType());
+                    writer.newLine();
+                    writer.write("Kategori: " + damage.getCategory());
+                    writer.newLine();
+                    writer.write("Pris: " + damage.getPrice() + " kr.");
+                    writer.newLine();
+                    writer.write("----------------------------------");
+                    writer.newLine();
+                }
+
+                writer.flush();
+            }
+
+        } catch (LeaseNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    return "printDamageReport";
+    }
+}
+
